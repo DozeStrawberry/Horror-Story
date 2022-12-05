b8278b2087e05ae7882bb838a9ee3c5fb293350d
DozeStrawberry <101461681+DozeStrawberry@users.noreply.github.com>
2022-12-05 16:19:20 +0800

    set up storyboard

---
+++ HorrorStory/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HorrorStory
+//
+//  Created by liu ya yun on 2022/12/3.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
