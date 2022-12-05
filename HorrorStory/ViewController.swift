bf0b0e999623907bf4c4ef41bbe33cf1b85d3c36
DozeStrawberry <101461681+DozeStrawberry@users.noreply.github.com>
2022-12-05 18:25:59 +0800

    scroll view can't use, change use tableView.

--- HorrorStory/ViewController.swift
+++ HorrorStory/ViewController.swift
@@ -8,7 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var channelNameLabel: UILabel!
+    
+    
+    @IBOutlet weak var SubscribersLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
